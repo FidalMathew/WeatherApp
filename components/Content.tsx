fd69328126b8bb105585f04ab18c638448255fd5
Fidal <fidal15perfect@gmail.com>
2023-02-17 02:08:16 +0530

    [feat]: add weather and forecast

---
+++ components/Content.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+
+  
+interface IWeather {
+    temp_c: number;
+    temp_f: number;
+    condition: string;
+    wind_kph: number;
+    humidity: number;
+  }
+
+function Content(props: any) {
+
+
+    // console.log(props.weather)
+    const changeTemp = () => {
+        props.setIsCelsius(!props.isCelsius);
+    }
+
+    function capitalizeFirstLetter(str : string) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    return (
+        <div>
+            <div className={styles.weatherDetails}>
+                <div>
+                    {
+                        props.isCelsius ? <div className={styles.head1}> {props.weather.temp_c} °C<button className={styles.tempButton} onClick={changeTemp}>|°F</button>  </div>
+                            : <div className={styles.head1}> {props.weather.temp_f} °F <button className={styles.tempButton} onClick={changeTemp}>|°C</button>  </div>
+                    }
+                <div className={styles.city}> {capitalizeFirstLetter(props.weather.finalCity)} </div>
+
+                </div>
+                <div className={styles.values}>
+                    <div>Condition: {props.weather.condition}</div>
+                    <div>Humidity: {props.weather.humidity}%</div>
+                    <div>Wind Speed: {props.weather.wind_kph}kph</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Content
---
+++ components/Forecast.tsx
@@ -0,0 +1,35 @@
+import styles from '../styles/Home.module.css'
+
+function Forecast(props: any) {
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    console.log("Forecast", props.forecast)
+  return (
+    <div className={styles.forecastContainer}>
+        {props.forecast.map((item: any, index: number) => {
+            
+            let d = new Date(item.date.toString());
+            let dayName = days[d.getDay()];
+
+            return (
+                <div key={index} className={styles.forecastCard}>
+                    <div className={styles.dayName}> { index==0?'Today':(index==1?'Tommorrow':dayName)} </div>
+                   <div> {item.condition}</div>
+                
+                {
+                    props.isCelsius?
+                    <div> {item.maxtemp_c}° {item.mintemp_c}° </div>
+                    :<div> {item.maxtemp_f}° {item.mintemp_f}° </div>
+                }
+
+                </div>
+            )
+        })
+            
+        }
+    </div>
+  )
+}
+
+export default Forecast
